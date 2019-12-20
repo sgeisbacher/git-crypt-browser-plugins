7935146fb29e439bb025256eeb0fbe5a42a51671
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2019-12-20 10:07:46 +0100

    introduced typescript

--- src/chrome/background.ts
+++ src/chrome/background.ts
@@ -1,19 +1,19 @@
-chrome.extension.getBackgroundPage().console.log('starting background 2 ...');
+console.log('starting background ...');
 
-const arrayBuffer2Hex = (buffer) => Array.prototype.map.call(buffer, (x) => ('00' + x.toString(16)).slice(-2)).join('');
+// const arrayBuffer2Hex = (buffer: Uint8Array): string => buffer.map((x) => ('00' + (x.toString(16).slice(-2)))).join('');
 
 const url = 'https://github.com/sgeisbacher/git-crypt-test/blob/master/text.txt?raw=true';
 fetch(url, { redirect: 'follow', headers: {}})
-    .then((resp, err) => resp.arrayBuffer())
+    .then((resp) => resp.arrayBuffer())
     .then((resp) => decrypt(new Uint8Array(resp)));
 
 const keyfilehexdata = '0047495443525950544b455900000002000000000000000100000004000000000000000300000020aefbbe588691ab9bde9953c93c738b14e4f99249f83b7509c115c2c5d15b585f000000050000004025aa91d937d04602ab97ebb24e7e5636332e8d17de8862c10011c2409a90937344348c05df063946815f3ee1ad94e7abf2a68f2f58fe6b7175aee93cfb548bfb00000000';
 
-const decrypt = async (cipherTextBytes) => {
+const decrypt = async (cipherTextBytes: Uint8Array) => {
     console.log('decrypting ...')
 
-    const hex2ArrayBuffer = (hex) => new Uint8Array(hex.match(/[\da-f]{2}/gi).map((h) => parseInt(h, 16)));
-    const createIV = (nonce) => new Uint8Array(16).map((v, i) => nonce[i] || 0)
+    const hex2ArrayBuffer = (hex: string): Uint8Array => new Uint8Array((hex.match(/[\da-f]{2}/gi) || []).map((h) => parseInt(h, 16)));
+    const createIV = (nonce: Uint8Array) => new Uint8Array(16).map((v, i) => nonce[i] || 0)
 
     const AES_KEY_START = 40;
     const AES_KEY_SIZE = 32;
@@ -40,7 +40,7 @@ const decrypt = async (cipherTextBytes) => {
     const key = await window.crypto.subtle.importKey(
             "raw",
             aeskey,
-            { name: "AES-CTR"},
+            "AES-CTR",
             false,
             ["encrypt", "decrypt"]
         )
