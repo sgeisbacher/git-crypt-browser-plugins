a4db377219c224d1af9dbc2917c0d6093d403694
Stefan Geisbacher <stefan.geisbacher@gmail.com>
2020-01-08 23:15:42 +0100

    switched from textaread-onChange to save-button in popup

--- src/chrome/popup.ts
+++ src/chrome/popup.ts
@@ -2,8 +2,9 @@ import {StoreMappingCommand} from './types';
 import {sendCommand} from './utils';
 
 const ta = document.querySelector<HTMLTextAreaElement>('#repoSecretMapping');
+const saveBtn = document.querySelector<HTMLButtonElement>('#repoSecretMappingSaveBtn');
 
-const onChange = () => {
+const onSave = () => {
     console.log('popup-change');
     const text = ta?.value;
     const cmd: StoreMappingCommand = {type: 'storeMapping', payload: {mapping: text || ''}};
@@ -11,7 +12,7 @@ const onChange = () => {
         console.log('success', response);
     });
 };
-ta?.addEventListener('change', onChange);
+saveBtn?.addEventListener('click', onSave);
 
 sendCommand({type: 'loadMapping', payload: {}}).then((resp) => {
     if (!ta || resp.type !== 'loadMapping') {
